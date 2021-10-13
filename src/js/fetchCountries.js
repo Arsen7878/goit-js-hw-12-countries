e580160014b3dc7f15f57a6093bae7ef1fdc82c3
Arsen Temurian <arsentim2611@gmail.com>
2021-10-14 02:44:33 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,10 +1,11 @@
 export default function fetchCountries(searchQuery) {
-  const a = fetch(`https://restcountries.com/v2/name/${searchQuery}`)
+  const fetchQuery = fetch(`https://restcountries.com/v2/name/${searchQuery}`)
     .then(response => {
       return response.json();
     })
     .then(data => {
       return data;
     });
-  return a;
+
+  return fetchQuery;
 }
